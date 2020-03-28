17f51e7f1416968d7473067c761449c77509f690
alexsupic <48335858+alexsupic@users.noreply.github.com>
2020-03-28 21:41:26 +0100

    Adds sample notebook

---
+++ hello.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 16 19:31:06 2019
+
+@author: User
+"""
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello World")
+    print(5+5)
+
+# Main program starts here
+hello()
